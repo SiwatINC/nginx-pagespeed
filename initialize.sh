cea2a45a68e55b76cd6b54c774ace6a97b96742e
Siwat2545 <siwat2545@cud55.com>
2019-02-03 00:20:01 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,16 @@
-mkdir -p /config/cache/pagespeed_cache
-mkdir -p /config/keys
-mkdir -p /config/log/nginx
-mkdir -p /config/log/pagespeed
-mkdir -p /config/log/php
-mkdir -p /config/nginx/site-confs
-mkdir -p /config/php
-mkdir -p /config/www
-mkdir -p /config/keys
+echo "Starting Container Initialization Script."
+if [ -z "$(ls -A /config)" ]; then
+   echo "Initializing . . ."
+   mkdir -p /config/cache/pagespeed_cache
+   mkdir -p /config/keys
+   mkdir -p /config/log/nginx
+   mkdir -p /config/log/pagespeed
+   mkdir -p /config/log/php
+   mkdir -p /config/nginx/site-confs
+   mkdir -p /config/php
+   mkdir -p /config/www
+   mkdir -p /config/keys
+else
+   echo "Already Initiated, Starting NGINX PageSpeed and PHP7.2"
+   exit 0
+fi
