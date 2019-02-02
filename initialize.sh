6414c408be537f466294ead075cd064fe0ec30d0
Siwat2545 <siwat2545@cud55.com>
2019-02-03 00:03:42 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,1 +1,9 @@
+mkdir -p /config/cache/pagespeed_cache
+mkdir -p /config/keys
+mkdir -p /config/log/nginx
+mkdir -p /config/log/pagespeed
+mkdir -p /config/log/php
+mkdir -p /config/nginx/site-confs
+mkdir -p /config/php
+mkdir -p /config/www
 
