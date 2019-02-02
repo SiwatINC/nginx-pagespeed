ef59acc8c8a1eaefb7e2477ae546527388afe4c9
Siwat2545 <siwat2545@cud55.com>
2019-02-03 00:26:11 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -10,6 +10,11 @@ if [ -z "$(ls -A /config)" ]; then
    mkdir -p /config/php
    mkdir -p /config/www
    mkdir -p /config/keys
+   curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/nginx.conf" > /config/nginx/nginx.conf
+   curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/fastcgi_params" > /config/nginx/fastcgi_params
+   curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/mime.types" > /config/nginx/mime.types
+   curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/default" > /config/nginx/site-confs/default
+   echo "; Add any php.ini entry here to overwrite any php.ini configuration" > /config/php/php-overwrite.ini
 else
    echo "Already Initiated, Starting NGINX PageSpeed and PHP7.2"
    exit 0
