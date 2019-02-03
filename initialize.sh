e23440a272f3722dc5cd33dc40b66a4c47db4c5e
Siwat2545 <siwat2545@cud55.com>
2019-02-03 13:47:27 +0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -15,10 +15,11 @@ if [ -z "$(ls -A /config)" ]; then
    curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/mime.types" > /config/nginx/mime.types
    curl "https://raw.githubusercontent.com/SiwatINC/nginx-pagespeed/master/default" > /config/nginx/site-confs/default
    echo "; Add any php.ini entry here to overwrite any php.ini configuration" > /config/php/php-overwrite.ini
-   echo "Please edit your site configuration in the /config/nginx/site-confs/default directory to enable pagespeed"
+   echo "Please replace yourdomain.com with your domain in /config/nginx/site-confs/default to enable pagespeed."
    exit 1
 else
    echo "Already Initiated, Starting NGINX PageSpeed and PHP7.2"
+   echo "If you haven't yet, please replace yourdomain.com with your domain in /config/nginx/site-confs/default to enable pagespeed."
    cat /config/php/php-overwrite.ini >> /etc/php/7.2/fpm/php.ini
    exit 0
 fi
